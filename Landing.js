077c6a801295625760915802f665b3af524e92b0
foreverherie <95851553+foreverherie@users.noreply.github.com>
2022-03-15 19:39:53 +0300

    Add files via upload

---
+++ Landing.js
@@ -0,0 +1,18 @@
+<script type ="text
+/ javascript">
+var images = new array ();
+function preload images (){
+				for i=0;i < preloadImages.length;1++)
+				
+				{ 
+								
+				images [i] = new image ();
+				image i =src preloadImages.arguments[i];
+				}
+}
+preload images ( );
+</script >
+
+<a>
+				href= "javascript:window.externalAcc 'Bonnets by Her ')" Add to Favorites 
+</a>
